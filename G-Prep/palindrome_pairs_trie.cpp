ecd3914d800ea6a42f7b096d743418f6f6b61fa0
Karthik Ragunath A <karthik@madstreetden.com>
2019-07-11 11:35:42 +0530

    Tries implementation

---
+++ G-Prep/palindrome_pairs_trie.cpp
@@ -0,0 +1,257 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+struct HASH
+{
+    size_t operator()(const pair< ll, ll > &pair_node) const
+    {
+        return hash< ll >()(pair_node.first ^ (pair_node.second << 32));
+    }
+};
+
+struct Trie
+{
+    unordered_map< char, Trie* > children;
+    vector< int > uptill;
+    vector< int > below;
+    vector< int > index_vec;
+};
+
+struct Trie_Graph
+{
+    Trie* root = new Trie;
+    void insert_nodes(string word, int index, vector< bool > &is_suffix_palindrome)
+    {
+        cout << "insert index: " << index << " word: " << word << "\n";
+        Trie* node = root;
+        int string_length = word.length();
+        for(int i = 0; i < string_length; i++)
+        {
+            char character = word[i];
+            auto child_finder = node -> children.find(character);
+            if(child_finder != node -> children.end())
+            {
+                node = node -> children[character];
+                node -> index_vec.push_back(index);
+            }
+            else
+            {
+                Trie* new_node = new Trie();
+                node -> children[character] = new_node;
+                node = new_node;
+                node -> index_vec.push_back(index);
+            }
+            if(i + 1 < string_length)
+            {
+                if(is_suffix_palindrome[i + 1])
+                {
+                    node -> below.push_back(index);
+                }
+            }
+        }
+        node -> uptill.push_back(index);
+        cout << "----------";
+    }
+
+    void search(string word, int index, vector< bool > &is_suffix_palindrome, unordered_map< int, int > &solution_vector)
+    {
+        cout << "search index: " << index << " word: " << word << "\n";
+        Trie* node = root;
+        int string_length = word.length();
+        int char_index = 0;
+        bool match_exists = true;
+        for(int i = 0; i < string_length; i++)
+        {
+            char character = word[i];
+            auto child_finder = node -> children.find(character);
+            if(child_finder != node -> children.end())
+            {
+                node = node -> children[character];
+                if(i + 1 < string_length)
+                {
+                    if(is_suffix_palindrome[i + 1])
+                    {
+                        for(auto uptill_index: node -> uptill)
+                        {
+                            solution_vector.insert(make_pair(uptill_index, index));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                match_exists = false;
+                break;
+            }
+        }
+        if(match_exists)
+        {
+            for(auto uptill_index: node -> uptill)
+            {    
+                solution_vector.insert(make_pair(uptill_index, index));
+            }
+            for(auto below_index: node -> below)
+            {
+                solution_vector.insert(make_pair(below_index, index));
+            }
+        }
+        cout << "*******" << "\n";
+    }
+};
+
+class Solution 
+{
+    public:
+        vector< bool > rolling_hash(string word)
+        {
+            vector< bool > is_index_palindrome;
+            int hash_modulo = 103;
+            is_index_palindrome.push_back(true);
+            if(word.length() == 1)
+            {
+                return is_index_palindrome;
+            }
+            int first_pointer = word[0] % hash_modulo;
+            int second_pointer = word[1] % hash_modulo;
+            int string_length = word.length();
+            for(int i = 1; i < string_length; i++)
+            {
+                cout << "first pointer: " << first_pointer << " second_pointer: " << second_pointer << "\n";
+                if(first_pointer == second_pointer)
+                {
+                    int first_half_length = (i + 1) / 2;
+                    int second_half_length = i + 1 - first_half_length;
+                    bool is_palindrome = true;
+                    for(int k = 0, j = i; k < first_half_length; k++, j--)
+                    {
+                        if(word[k] != word[j])
+                        {
+                            is_palindrome = false;
+                            break;
+                        }
+                    }
+                    if(is_palindrome)
+                    {
+                        is_index_palindrome.push_back(true);
+                    }
+                    else
+                    {
+                        is_index_palindrome.push_back(false);
+                    }
+                }
+                else
+                {
+                    is_index_palindrome.push_back(false);
+                }
+                if(i + 1 < string_length)
+                {
+                    if(i % 2 == 0)
+                    {
+                        first_pointer += (word[(i + 1) / 2] % hash_modulo);
+                        second_pointer += (word[(i + 1)] % hash_modulo);
+                        first_pointer = first_pointer % hash_modulo;
+                        second_pointer = second_pointer % hash_modulo;
+                    }
+                    else
+                    {
+                        second_pointer = second_pointer - (word[(i + 1) / 2] % hash_modulo) + (word[i + 1] % hash_modulo);
+                        second_pointer = (second_pointer % hash_modulo);
+                    }
+                }
+            }
+            return is_index_palindrome;
+        }
+
+        void print_suffix_palindromes(vector< string > &words)
+        {
+            int words_size = words.size();
+            for(int i = 0; i < words_size; i++)
+            {
+                string rev_string(words[i]);
+                reverse(rev_string.begin(), rev_string.end());
+                auto palindromic_suffixes = rolling_hash(rev_string);
+                int palindromic_suffixes_length = palindromic_suffixes.size();
+                for(int j = palindromic_suffixes_length - 1, k = 0; j >= 0; j--, k++)
+                {
+                    cout << "string: " << i + 1 << " suffix string: " << words[i].substr(j, k + 1) << " is a " << ((palindromic_suffixes[k] == true) ? "true" : "false") << "\n";
+                }
+                cout << "----------" << "\n";
+            }
+        }
+
+        vector<vector<int>> palindromePairs(vector<string>& words) 
+        {
+            // print_suffix_palindromes(words);
+             int words_vec_length = words.size();
+             cout << "mile_stone 2: " << words_vec_length << "\n";
+             
+            unordered_map< int, int > solution_vector;
+
+            Trie_Graph* trie_graph = new Trie_Graph;
+            for(int i = 0; i < words_vec_length; i++)
+            {
+                string word = words[i];
+                cout << "index: " << i << " word: " << word << "\n";
+                auto palindromic_prefixes = rolling_hash(word);
+                string rev_word(word); //copy string
+                reverse(rev_word.begin(), rev_word.end());
+                cout << "rev word: " << rev_word << "\n";
+                vector< bool > palindromic_prefixes_inversed(palindromic_prefixes);
+                reverse(palindromic_prefixes_inversed.begin(), palindromic_prefixes_inversed.end());
+                // trie_graph -> search(rev_word, i, palindromic_prefixes_inversed, solution_vector);
+                auto palindromic_suffixes_inversed = rolling_hash(rev_word);
+                vector< bool > palindromic_suffixes(palindromic_suffixes_inversed);
+                reverse(palindromic_suffixes.begin(), palindromic_suffixes.end());
+                // trie_graph -> insert_nodes(word, i, palindromic_suffixes);
+            }
+
+            trie_graph = new Trie_Graph;
+            for(int i = words_vec_length - 1; i >= 0; i--)
+            {
+                string word = words[i];
+                auto palindromic_prefixes = rolling_hash(word);
+                string rev_word(word); //copy string
+                reverse(rev_word.begin(), rev_word.end());
+                vector< bool > palindromic_prefixes_inversed(palindromic_prefixes);
+                reverse(palindromic_prefixes_inversed.begin(), palindromic_prefixes_inversed.end());
+                // trie_graph -> search(rev_word, i, palindromic_prefixes_inversed, solution_vector);
+                auto palindromic_suffixes_inversed = rolling_hash(rev_word);
+                vector< bool > palindromic_suffixes(palindromic_suffixes_inversed);
+                reverse(palindromic_suffixes.begin(), palindromic_suffixes.end());
+                // trie_graph -> insert_nodes(word, i, palindromic_suffixes);
+            }
+
+            vector< vector< int > > palindromic_pairs;
+            for(auto map_iter = solution_vector.begin(); map_iter != solution_vector.end(); map_iter)
+            {
+                int index_1 = map_iter -> first;
+                int index_2 = map_iter -> second;
+                vector< int > pair_vec;
+                pair_vec.push_back(index_1);
+                pair_vec.push_back(index_2);
+                palindromic_pairs.push_back(pair_vec);
+            }
+            return palindromic_pairs;
+        }
+};
+
+int main()
+{
+    Solution solution;
+    vector< string > words;
+    words.push_back("bat");
+    words.push_back("tab");
+    words.push_back("cat");
+    // solution.print_suffix_palindromes(words);
+    cout << "mile stone 1" << "\n";
+    solution.palindromePairs(words);
+    // Trie_Graph* trie_graph = new Trie_Graph;
+    // vector< bool > test;
+    // test.push_back(0);
+    // test.push_back(0);
+    // test.push_back(1);
+    // trie_graph -> insert_nodes("bat", 0, test);
+    // trie_graph -> insert_nodes("tab", 0, test);
+    return 0;
+}
--- G-Prep/serialize_and_deserialize_tree.cpp
+++ G-Prep/serialize_and_deserialize_tree.cpp
@@ -7,60 +7,92 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Codec {
-public:
+class Codec 
+{
+    public:
 
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        string serializer_solution = "[";
-        queue< TreeNode* > serializer_queue;
-        serializer_queue.push(root);
-        while(!serializer_queue.empty())
-        {
-            TreeNode* tree_node = serializer_queue.front();
-            serializer_queue.pop();
-            if(tree_node != NULL)
+        // Encodes a tree to a single string.
+        string serialize(TreeNode* root) {
+            string serializer_solution = "[";
+            queue< TreeNode* > serializer_queue;
+            serializer_queue.push(root);
+            while(!serializer_queue.empty())
             {
-                serializer_queue.push(tree_node -> left);
-                serializer_queue.push(tree_node->right);
-                serializer_solution += to_string(tree_node -> val);
-                serializer_solution += ",";
+                TreeNode* tree_node = serializer_queue.front();
+                serializer_queue.pop();
+                if(tree_node != NULL)
+                {
+                    serializer_queue.push(tree_node -> left);
+                    serializer_queue.push(tree_node-> right);
+                    serializer_solution += to_string(tree_node -> val);
+                    serializer_solution += ",";
+                }
+                else
+                {
+                    serializer_solution += "null,";
+                }
+            }
+            auto last_char_iter = serializer_solution.rbegin();
+            if(*last_char_iter == ',')
+            {
+                serializer_solution.erase(serializer_solution.end() - 1);
+            }
+            serializer_solution += ']';
+            // cout << serializer_solution << "\n";
+            return serializer_solution;
+        }
+
+        // Decodes your encoded data to tree.
+        TreeNode* deserialize(string data) 
+        {
+            TreeNode* root_node = new TreeNode(0);
+            data.erase(data.begin());
+            data.erase(data.end() - 1);
+            vector<string> tree_vals;
+            stringstream ss(data);
+            string token;
+            while(getline(ss, token, ','))
+            {
+                tree_vals.push_back(token);
+            }
+            // for(auto token: tree_vals)
+            // {
+            //     cout << token << "\n";
+            // }
+            if(tree_vals.size() >= 0 and tree_vals[0] != "null")
+            {
+                root_node -> val = stoi(tree_vals[0]);
+                root_node -> left = NULL;
+                root_node -> right = NULL;
+                queue< TreeNode* > bfs_tree;
+                bfs_tree.push(root_node);
+                int tree_vals_length = tree_vals.size();
+                for(int i = 0; i < tree_vals_length;)
+                {
+                    TreeNode* node = bfs_tree.front();
+                    bfs_tree.pop();
+                    if(i + 1 < tree_vals_length and tree_vals[i + 1] != "null")
+                    {
+                        TreeNode* node_left = new TreeNode(stoi(tree_vals[i + 1]));
+                        node -> left = node_left;
+                        bfs_tree.push(node -> left);
+                    }
+                    if(i + 2 < tree_vals_length and tree_vals[i + 2] != "null")
+                    {
+                        TreeNode* node_right = new TreeNode(stoi(tree_vals[i + 2]));
+                        node -> right = node_right;
+                        bfs_tree.push(node -> right);
+                    }
+                    i += 2;
+                }
             }
             else
             {
-                serializer_solution += "null,";
+                root_node = NULL;
             }
+            
+            return root_node;
         }
-        auto last_char_iter = serializer_solution.rbegin();
-        if(*last_char_iter == ',')
-        {
-            // serializer_solution.end() - 1
-            serializer_solution.erase(serializer_solution.end() - 1);
-        }
-        serializer_solution += ']';
-        cout << serializer_solution << "\n";
-        return serializer_solution;
-    }
-
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        TreeNode* new_node = new TreeNode;
-        data.erase(data.begin());
-        data.erase(data.end() - 1);
-        vector<string> tree_vals;
-        stringstream ss(data);
-        string token;
-        while(getline(ss, token, ','))
-        {
-            tree_vals.push_back(token);
-        }
-        // for(auto token: tree_vals)
-        // {
-        //     cout << token << "\n";
-        // }
-        
-        return new_node;
-    }
 };
 
 // Your Codec object will be instantiated and called as such:

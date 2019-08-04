97cbab59e857d6c6d0720914ebc2b2135dd1f8b4
Karthik Ragunath A <karthik@madstreetden.com>
2019-08-04 06:04:53 +0530

    mos initial commit

---
+++ G-Prep/Mos_Algo_Sqrt_Decomp.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+ll block_size = sqrt(30000);
+class ComparePairs
+{
+	bool operator()(const pair< ll, ll > &pair_val_1, const pair< ll, ll > &pair_val_2) const
+	{
+		if(pair_val_1.first / block_size < pair_val_2.first / block_size)
+		{
+			return true;
+		}
+		else if(pair_val_1.first / block_size == pair_val_2.first / block_size)
+		{
+			if(pair_val_1.second < pair_val_2.second)
+			{
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+};
+
+ll distinct_vals = 0;
+
+void add(ll val)
+{
+	distinct_val_map[val] += 1;
+	if(distinct_val_map[val] == 1)
+	{
+		distinct_vals += 1;
+	}
+}
+
+void remove(ll val)
+{
+	distinct_val_map[val] -= 1;
+	if(distinct_val_map[val] == 0)
+	{
+		distinct_vals -= 1;
+	}
+}
+
+unordered_map< ll, ll > distinct_val_map;
+unordered_map< ll, ll > solution;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll inputs;
+	cin >> inputs;
+	ll queries;
+	cin >> queries;
+	vector< pair< ll, ll > > queries;
+	for(ll i = 0; i < queries; i++)
+	{
+		int q1, q1;
+		queries.push_back(make_pair(q1 - 1, q2 - 1));
+	}
+	sort(queries.begin(), queries.end(), ComparePairs());
+	ll current_left = 0, current_right = 0;
+	for(ll i = 0; i < queries; i++)
+	{
+		ll left = queries[i].first;
+		ll right = queries[i].second;
+		while(current_left < left)
+		{
+			remove(current_left);
+			current_left += 1;
+		}
+		while(left < current_left)
+		{
+			add(current_left);
+			current_left -= 1;
+		}
+		while(current_right < right)
+		{
+			add(current_right);
+			current_right += 1;
+		}
+		while(right < current_right)
+		{
+			remove(current_right);
+			current_right -= 1;
+		}
+	}
+	return 0;
+}

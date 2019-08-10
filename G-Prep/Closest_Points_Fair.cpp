bac1f80107e80eca9d3d1ec590ac8a42eb3e5f8b
Karthik Ragunath A <karthik@madstreetden.com>
2019-08-10 07:33:12 +0530

    Sweep lines Closest Pair Distance Algorithm Done

---
+++ G-Prep/Closest_Points_Fair.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+class CompareVector
+{
+	public:
+		bool operator()(const pair< double, double > &pair_1, const pair< double, double > &pair_2) const
+		{
+			return pair_1.first < pair_2.first;
+		}
+};
+
+double closest_points_distance(vector< pair< double, double > > &input_points)
+{
+	vector< pair< double, double > > new_points(input_points);
+	sort(input_points.begin(), input_points.end(), CompareVector());
+	set< pair< ll, ll > > bounding_box;
+	bounding_box.insert(input_points[0]);
+	double min_distance = 10000000000000;
+	for(int i = 1; i < input_points.size(); i++)
+	{
+		int left = 0;
+		for(auto iter = new_points.begin(); iter != new_points.end() and left < i; left++)
+		{
+			if(iter -> first + min_distance < input_points[i].first)
+			{
+				bounding_box.erase(*iter);
+				new_points.erase(iter); //erase a non existing value in set doesnt throw an error in sets
+			}
+			else
+			{
+				break;
+			}
+			left += 1;
+		}
+		for(auto iter = bounding_box.lower_bound(make_pair(input_points[i].first - min_distance, input_points[i].second - min_distance)); iter != bounding_box.end() and (input_points[i].second + min_distance >= iter -> second); iter++)
+		{
+			min_distance = min(min_distance, sqrt(pow(input_points[i].first - iter -> first, 2) + pow(input_points[i].second - iter -> second, 2)));
+		}
+		bounding_box.insert(input_points[i]);
+	}
+	return min_distance;
+}
+
+int main()
+{
+	// vector< pair< double, double > > input_points{make_pair(2, 3), make_pair(12, 30), make_pair(40, 50), make_pair(5, 1), make_pair(12, 10), make_pair(3, 4)};
+	vector< pair< double, double > > input_points{make_pair(12, 13), make_pair(12, 30), make_pair(18, 16), make_pair(20, 13), make_pair(24, 25), make_pair(8, 7)};
+	double min_distance = closest_points_distance(input_points);
+	cout << min_distance << "\n";
+	return 0;
+}
--- G-Prep/closest_points_sweep_lines.cpp
+++ G-Prep/closest_points_sweep_lines.cpp
@@ -1,8 +1,10 @@
-// https://leetcode.com/problems/find-k-th-smallest-pair-distance/
-
+// https://leetcode.com/problems/find-k-th-smadoubleest-pair-distance/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int 
 class CompareNodes
 {
-	bool operator(const pair< int, int > &pair_1, const pair< int, int > &pair_2) const
+	bool operator(const pair< double, double > &pair_1, const pair< double, double > &pair_2) const
 	{
 		return pair_1.first < pair_2.first;
 	}
@@ -10,24 +12,52 @@ class CompareNodes
 
 class CompareSets
 {
-	bool operator()(const pair< ll, ll > &pair_1, const pair< ll, ll > &pair_2) const
+	bool operator()(const pair< double, double > &pair_1, const pair< double, double > &pair_2) const
 	{
 		return pair_1.second < pair_2.second;
 	}
-}
+};
 
 class Solution {
 public:
-    int smallestDistancePair(vector< pair< int, int > > &input_pairs) 
+	bool cmp(double compare_value, const pair< double, double > &pair_val)
+	{
+		// auto upper_bound_iter = upper_bound(input_pairs.begin(), input_pairs.end(), compare_value, cmp); //Upper bound or lower bound in sorted vector
+		if(pair_val.first > compare_value and pair_val.second > compare_value)
+		{
+			return true;
+		}
+	}
+
+    double shortest_pair(vector< pair< double, double > > &input_pairs) 
     {
     	sort(input_pairs.begin(), input_pairs.end(), CompareNodes());
-    	set< pair< ll, ll >, CompareSets > visited_set;
+    	set< pair< double, double > > visited_set; // default sort by x and then sort by y
     	visited_set.insert(input_pairs[0]);
-    	int max_distance = 1000000000;
-    	for(int i = 1; i < input_pairs; i++)
+    	double min_distance = 10000000000000;
+    	double left = 0;
+    	for(double i = 1; i < input_pairs.size(); i++)
     	{
-
+    		while(left < i && (input_pairs[i].first - input_pairs[left].first) > min_distance)
+    		{
+    			visited_set.erase(input_pairs[left]);
+    			left += 1;
+    		}
+    		// default lower_bound -> return pair.first >= value and pair.second >= value;
+    		for(auto iter = visited_set.lower_bound(make_pair(input_pairs[i].first - min_distance, input_pairs[i].second - min_distance)); iter != visited_set.end() and input_pairs[i].second + min_distance >= iter -> second; iter++) // lower_bound takes care of left and below and conditional takes care of left and above
+    		{
+    			min_distance = min(min_distance, sqrt(pow((input_pairs[i].first - iter -> first), 2) + pow((input_pairs[i].second - iter -> second), 2)));
+    		}
+    		visited_set.insert(input_pairs[i]);
     	}
+    	return min_distance;
     }
-
 };
+
+int main()
+{
+	Solution solution;
+	vector< pair< double, double > > input_pairs{make_pair(2, 3), make_pair(12, 30), make_pair(40, 50), make_pair(5, 1), make_pair(12, 10), make_pair(3, 4)};
+	double min_distance = solution.shortest_pair(inout_pairs);
+	cout << min_distance << "\n";
+}
